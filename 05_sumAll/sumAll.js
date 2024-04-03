b365f2df5d4c916df71a2767d50e423041787608
Nick <75218083+NickBlust@users.noreply.github.com>
2024-04-03 22:38:42 +0200

    feat: complete fifth javascript assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
 
+    if (b < a) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+
+    result = 0;
+    for (i = a; i <= b; i++)
+        result += i;
+    return result;
 };
 
 // Do not edit below this line
