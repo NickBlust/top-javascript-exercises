95cc519939d79a4843646c998e1912df6f937c02
Nick <75218083+NickBlust@users.noreply.github.com>
2024-04-03 21:37:49 +0200

    feat: complete second javascript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (s) {
+    if (typeof s !== "string") return "ERROR";
 
+    // 'gnirts' is 'string' reversed
+    gnirts = "";
+    for (let i = 0; i < s.length; i++)
+        gnirts += s[s.length - 1 - i];
+    return gnirts;
 };
 
 // Do not edit below this line
