350d1a0b7deebca75f4f20cdb8b3ad3bcb4cb270
Nick <75218083+NickBlust@users.noreply.github.com>
2024-04-03 22:16:26 +0200

    feat: complete fourth javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, num) {
 
+    for (let i = 1; i < arguments.length; i++) {
+        arr = arr.filter((item) => item !== arguments[i]);
+    }
+    console.log(arr);
+    return arr;
+    // if (!Array.isArray(arr)) return "ERROR";
+    // if(Number.isNaN(num) === NaN || typeof num === "undefined") num = 1;
+    // console.log(num);
+    // arr.splice(pos - 1, num);
+    // console.log(arr);
+    // return arr;
 };
 
 // Do not edit below this line
