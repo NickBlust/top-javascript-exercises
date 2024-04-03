f8f3921ed0332c06e659deb944fe2340b3720e4b
Nick <75218083+NickBlust@users.noreply.github.com>
2024-04-03 21:31:07 +0200

    feat: complete second javascript assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (s, count) {
+    if (count < 0)
+        return "ERROR";
 
+    result = "";
+    for (let i = 0; i < count; i++)
+        result += s;
+    return result;
 };
 
 // Do not edit below this line
