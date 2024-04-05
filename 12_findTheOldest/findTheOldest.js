c1d21448f93cf51e2c92a8f0104dd593b30a0ac4
Nick <75218083+NickBlust@users.noreply.github.com>
2024-04-05 17:59:35 +0200

    feat: almost complete twelvth javascript assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    for (person in people)
+        if (!person[yearOfDeath])
+            person.yearOfDeath = 2024;
+    return people.sort((p1, p2) => (p1.yearOfDeath - p1.yearOfBirth) - (p2.yearOfDeath - p2.yearOfBirth)).slice(-1)[0];
 };
 
 // Do not edit below this line
