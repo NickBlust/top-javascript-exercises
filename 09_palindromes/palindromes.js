9a09534b236505534336d1476f06d2a77062eee6
Nick <75218083+NickBlust@users.noreply.github.com>
2024-04-05 17:38:34 +0200

    feat: complete ninth javascript assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    if (typeof s !== "string") return "ERROR";
 
+    s = s.replaceAll('!', '');
+    s = s.replaceAll('?', '');
+    s = s.replaceAll(',', '');
+    s = s.replaceAll('.', '');
+    s = s.replaceAll(' ', '');
+    s = s.toLowerCase();
+
+    r = s.split('').reverse().join('');
+    return s === r;
 };
 
 // Do not edit below this line
